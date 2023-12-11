fa0c542ab45a49b47b71c82d46b2616278ca210b
Matt <65626476+matthew-gee@users.noreply.github.com>
2023-12-11 23:47:20 +0000

    Index updated

--- index.php
+++ index.php
@@ -15,10 +15,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Orbitron:wght@600&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="css/styles.min.css">
-</head>
-
-<script src="bootstrap-5.3.2-dist/js/bootstrap.bundle.js"></script>
+    <link rel="stylesheet" href="css/styles.min.css"></head>
 
 <!-- Navbar -->
 <nav class="navbar navbar-fixed-top navbar-expand-lg p-3 mb-3 bg-primary">
@@ -57,33 +54,92 @@
 </nav>
 
 
-<body>
-    <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
-    <div class="carousel-inner">
-        <div class="carousel-item active">
-        <img src="images/slide1.jpg" class="d-block w-100" alt="...">
-        </div>
-        <div class="carousel-item">
-        <img src="images/slide1.jpg" class="d-block w-100" alt="...">
-        </div>
-        <div class="carousel-item">
-        <img src="images/slide1.jpg" class="d-block w-100" alt="...">
-        </div>
+<div id="carouselExample" class="carousel slide">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="images/slide1.png" class="d-block w-100" alt="...">
     </div>
-    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Previous</span>
-    </button>
-    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Next</span>
-    </button>
+    <div class="carousel-item">
+      <img src="images/slide2.png" class="d-block w-100" alt="...">
     </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+
+<div class="container">
+  <div class="row mt-5">
+      <div class="col pb-5">
+          <img class="mt-5"src="images/sportscentre.jpg" alt="" height="400" width="550">
+          <br>
+      </div>
+      <div class="col mb-5 fs-4 mr-3">
+        <h2 class="mt-5 mb-5"><strong>About Us</strong></h2>
+          <p class="mt-5"><strong>Zenith Sports Hub</strong> is more than a sports center; 
+            it's a community-driven space dedicated to excellence in fitness 
+            and athletic achievement. Our state-of-the-art facilities and 
+            expert trainers cater to individuals of all levels and ages. 
+            We believe in the transformative power of sports and aim to 
+            create an environment where you can push your limits, achieve 
+            your fitness goals, and be a part of a vibrant community. 
+            Join us at Zenith Sports Hub, where passion meets performance!</p>
+      </div>
+  </div>
+
+  <hr style="border-width: 2px;">
+
+  <div class="row">
+      <div class="col mb-5 fs-4 mr-3">
+      <h2 class="mt-5 mb-5"><strong>Contact us</strong></h2>
+          <p class="mt-5">Come find us at <strong>Zenith Sports Hub</strong>, Unit E4, Arena Business Centre, Holyrood CL, Poole BH17 7FJ<br>
+          <br>
+            <strong>Email:</strong> info@ZenithSports.com<br>
+            <strong>Phone Number:</strong> 07904111242<br>
+            <strong>Instagram:</strong> @ZenithSports<br>
+            <strong>X:</strong> @ZenithSportsHub<br>
+            <strong>Facebook:</strong> @ZenithSports
+          </p>
+      </div>
+      <div class="col">
+      <iframe class="mt-3"src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d315.5945681257861!2d-1.9939281199011423!3d50.74300415024202!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4873a716f2ae0daf%3A0x7b4553d762ce0bd1!2sUnit%20E4!5e0!3m2!1sen!2suk!4v1702326231677!5m2!1sen!2suk" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>  </div>
+
+  <hr style="border-width: 2px;">
+
+  <div class="row text-center">
+    <div class="col mb-5 fs-4">
+        <h2 class="mt-5 mb-5">Get in touch!</h2>
+        <p>Got a question or request? Let us know, and we will reply as soon as possible!</p>
+        <form>
+            <div class="row justify-content-center mb-3">
+                <div class="col-md-5 mb-3">
+                    <input type="text" class="form-control" placeholder="Your Name">
+                </div>
+                <div class="col-md-5 mb-3">
+                    <input type="text" class="form-control" placeholder="Your Email">
+                </div>
+            </div>
+            <div class="row justify-content-center mb-3">
+                <div class="col-md-8">
+                    <input type="textarea" class="form-control" style="height: 150px;" placeholder="Your Message">
+                </div>
+            </div>
+            <div class="row justify-content-center">
+                <div class="col-md-5">
+                    <button class="btn btn-primary text-white" style="width: 100%;">Submit</button>
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
 
 
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 
-
- 
 </body>
 
 
